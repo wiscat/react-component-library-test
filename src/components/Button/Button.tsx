29d3a006fbb98705bbfd7fe6b681735ad1df6a60
Oleg Pecherin <requirunt@gmail.com>
2022-05-19 12:34:04 +0300

    up

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+// @ts-ignore
+import styles from "./Button.css";
+
 export interface ButtonProps {
     label: string;
 }
 
 const Button = (props: ButtonProps) => {
-    return <button>{props.label}</button>;
+    return <button className={styles.btn}>{props.label}</button>;
 };
 
 export default Button;
